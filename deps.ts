c985e2db0fd461457d244fe95320b707bb6287be
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2022-11-16 12:18:15 +0900

    chore(deps): up deps version

--- deps.ts
+++ deps.ts
@@ -4,9 +4,9 @@ export {
   isString,
 } from "https://deno.land/x/isx@1.0.0-beta.24/mod.ts";
 export { prop } from "https://deno.land/x/prelude_js@1.0.0-beta.3/mod.ts";
-export { maxBy } from "https://deno.land/std@0.153.0/collections/max_by.ts";
+export { maxBy } from "https://deno.land/std@0.163.0/collections/max_by.ts";
 export { mapValues } from "https://deno.land/std@0.163.0/collections/map_values.ts";
-export { distinct } from "https://deno.land/std@0.153.0/collections/distinct.ts";
+export { distinct } from "https://deno.land/std@0.163.0/collections/distinct.ts";
 
 // deno-lint-ignore ban-types
 export type ExtendArg<F extends Function, Arg> = F extends
